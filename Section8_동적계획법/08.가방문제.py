38475b806279c08cdcb659bb1ba277a4c0490a14
Marc416 <juni416@naver.com>
2020-09-04 18:50:11 +0900

    DP

--- Section8_동적계획법/08.가방문제.py
+++ Section8_동적계획법/08.가방문제.py
@@ -1,16 +1,12 @@
 import sys
-
+# 냅색알고리즘
 sys.stdin = open('08.txt', 'rt')
 n, m = map(int, input().split())
-li = list(list(map(int, input().split())) for _ in range(n))
-# 두번째 원소를 기준으로 다차원배열 정렬
-li.sort(key=lambda x: x[1], reverse=True)
-ch = [0] * n
-# 가치
-ch[0] = li[0][1]
-# 무게
-kg = li[0][0]
-for i in range(1, n):
-    if kg + li[i][0] < m:
-        ch[i] = ch[i - 1] + li[i][1]
-        kg += li[i][0]
+dy = [0] * (m+1)
+for i in range(n):
+    # weight, value
+    w, v = map(int, input().split())
+    # 현재 보고자하는 보석의 무게부터 j for문을 돌린다
+    for j in range(w, m+1):
+        dy[j] = max(dy[j], dy[j-w] + v)
+print(dy[m])
---
+++ Section8_동적계획법/10.동전교환.py
@@ -0,0 +1,18 @@
+# 최소값을 찾는 것이므로 dy에는 큰값으로 저장이 되어야 한다
+# 가방문제와 같으나 최대값이냐 최소값이냐에 따라 다르다
+import sys
+sys.stdin = open('10.txt', 'rt')
+n = int(input())
+coin = list(map(int, input().split()))
+m = int(input())
+dy = [1000]*(m+1)
+dy[0] = 0
+# 동전이 세개 있으므로 처음에 한개 있을때를 경우로
+# 다 적용해보고 그다음 두개 째때를 적용해보고고for i in range(1, n+1):
+for i in range(n):
+    for j in range(coin[i], m + 1):
+        # j만큼의 거스름 돈을 주려고 할 때
+        # coin[i]를 반드시 쓴다고 가정할 때 동전 +1
+        dy[j] = min(dy[j-coin[i]] + 1, dy[j])
+
+print(dy[m])
